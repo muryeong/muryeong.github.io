f6c1d857cb7f0ff0edb66469fda51f9f9527221f
muryeong <stillfallen82@gmail.com>
2026-03-25 02:47:38 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+function chat(){
+
+let input=document.getElementById("userInput").value
+
+let reply="Muryeong AI is thinking about: "+input
+
+document.getElementById("response").innerText=reply
+
+}
+
+function double(){
+
+let n=document.getElementById("num").value
+
+document.getElementById("result").innerText=n*2
+
+}
